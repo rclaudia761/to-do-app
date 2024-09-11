697c969efaa1fd35d3d956b6331bbccf7d5ef04e
rclaudia761 <rclaudia761@gmail.com>
2024-09-11 15:42:32 +0530

    minor chage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { useContext } from 'react';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import './global.css'; // Ensure global styles are imported
 import Dashboard from './Screens/Dashboard';
 import ToDoList from './Screens/ToDoList';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
   return (
@@ -19,26 +16,6 @@ function App() {
           <Route path="/toDoList" element={<ToDoList />} />
         </Routes>
       </Router>
-
-        {/* <MainComponent />
-
-  <Dashboard/>
-  <ToDoList /> */}
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </ThemeProvider>
    
   );
@@ -55,22 +32,10 @@ const Navbar = () => {
       <button onClick={toggleTheme }style={{float:'right'}}>
        {`Switch to: ${theme === 'light'? 'dark':'light'}`}
       </button>
-      {/* <ThemeSwitcher/> */}
     </nav>
   );
 };
 
-// const MainComponent = () => {
-//   const { theme, toggleTheme } = useTheme();
 
-//   return (
-//     <div className={`app ${theme}`}>
-//       <h1>{`Current Theme: ${theme}`}</h1>
-//       <button onClick={toggleTheme}>
-//         Toggle Theme
-//       </button>
-//     </div>
-//   );
-// };
 
 export default App;
